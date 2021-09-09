13775554a3d0b9bc55ddc69d00a7f6ea839a85d8
Do Minh Huy <dominhhuy200017@gmail.com>
2021-09-09 14:06:29 +0700

    moving form root folder to right folder for each file

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,12 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+
+Route::get('/home', function(){
+    return Inertia::render('Home');
+})->name('home');
+
+Route::get('/topic', function () {
+    return Inertia::render('Topic');
+})->name('topic');
